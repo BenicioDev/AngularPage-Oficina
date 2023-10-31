02fc2e35784380b65e23768003234f57289f457d
Gabriel Benicio <gabrielvitor083@gmail.com>
2023-10-30 22:55:30 -0300

    Main content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/body-components/main-content/main-content.component';
+import { RowComponentComponent } from './components/body-components/row-component/row-component.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainContentComponent,
+    RowComponentComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/body-components/row-component/row-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-row-component',
+  templateUrl: './row-component.component.html',
+  styleUrls: ['./row-component.component.css']
+})
+export class RowComponentComponent {
+
+}
